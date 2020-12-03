5b8a35b39331b0d456c70235e742e4fcb6700a41
exel14 <Showmetheway220@gmail.com>
2020-12-03 19:07:58 +0600

    Create Serializer: DocumentSerializer AccountRegisterSerializer UserSerializer

---
+++ ChamberOfSecret/HP/serializer.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from .models import *
+from django.contrib.auth.models import User
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
+
+
+class AccountRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['name','last_name','phone']
+
+class UserSerializer(serializers.ModelSerializer):
+    account = AccountRegisterSerializer()
+
+    class Meta:
+        model = User
+        fields = ['username','password','account']
+
+    def create(self, validated_data):
+        password = self.validated_data.get('password')
+        profile_data = validated_data.pop('account')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        Account.objects.create(user=user, **profile_data)
+        return user
