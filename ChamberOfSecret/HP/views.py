cf559614c9683affe2125ea0596c12e851e04809
exel14 <Showmetheway220@gmail.com>
2020-12-03 19:10:34 +0600

    Add new view: DocumentView AccountRegisterView AccountLoginView

--- ChamberOfSecret/HP/views.py
+++ ChamberOfSecret/HP/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import *
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from .serializer import *
+from rest_framework import status,viewsets,filters,views,generics
 
-# Create your views here.
+
+class DocumentsView(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+
+class AccountRegisterView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class AccountLoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                            context={'request':request})
+        if not serializer.is_valid():
+            return Response({'User not found'})
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token':token.key
+        })
+
+
+
