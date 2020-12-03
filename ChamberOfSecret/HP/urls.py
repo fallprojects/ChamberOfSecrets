88c3a57bcabff1086bd39abc3dfb4398cb8dcc46
exel14 <Showmetheway220@gmail.com>
2020-12-03 19:09:58 +0600

    add url for documents, register, auth

--- ChamberOfSecret/HP/urls.py
+++ ChamberOfSecret/HP/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from .views import *
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register('documents',DocumentsView)
+router.register('register',AccountRegisterView)
+
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('',include(router.urls)),
+    path('auth',AccountLoginView.as_view(),name='auth')
 ]
