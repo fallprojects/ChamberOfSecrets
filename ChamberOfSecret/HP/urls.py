409d1da318a29e43e3cee656e33ab7ee5d8ad53a
exel14 <Showmetheway220@gmail.com>
2020-12-08 17:28:44 +0600

    add new url document-filter

--- ChamberOfSecret/HP/urls.py
+++ ChamberOfSecret/HP/urls.py
@@ -3,11 +3,12 @@ from django.urls import path,include
 from .views import *
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register('documents',DocumentsView)
+router.register('documents',DocumentView)
 router.register('register',AccountRegisterView)
 
 
 urlpatterns = [
     path('',include(router.urls)),
-    path('auth',AccountLoginView.as_view(),name='auth')
+    path('login/',AccountLoginView.as_view(),name='login'),
+    path('document_filter/', DocumentViewFilter.as_view(), name='document_filter')
 ]
