d1e3bdea5d01479bb6314229e73f0881ff4dd755
exel14 <Showmetheway220@gmail.com>
2020-12-08 17:29:12 +0600

    Add DocumentView DocumentViewFilter

--- ChamberOfSecret/HP/views.py
+++ ChamberOfSecret/HP/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 from .models import *
+from django.contrib.auth.decorators import login_required
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
@@ -8,13 +10,19 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .serializer import *
 from rest_framework import status,viewsets,filters,views,generics
+from .decorators import *
 
-
-class DocumentsView(viewsets.ModelViewSet):
+class DocumentView(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
 
+class DocumentViewFilter(APIView):
+    
+    def get(self):
+        documents = Document.objects.all()
+        serializer = DocumentSerializer(documents)
 
+        return Response(serializer.data)
 class AccountRegisterView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
