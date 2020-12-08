d3a5ae39278b759bdab1d89e3aba0de8cf479562
exel14 <Showmetheway220@gmail.com>
2020-12-08 17:26:49 +0600

    Add new decorator

--- ChamberOfSecret/HP/decorators.py
+++ ChamberOfSecret/HP/decorators.py
@@ -1,3 +1,12 @@
+
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import Group
+
+# def unauth_user(view_funk):
+#         def wrapped(request,*args,**kwargs):
+#             if request.user.is_authenticated:
+#                 return redirect('HP')
+#             else:
+#                 return view_funk(request,*args,**kwargs)
+#         return wrapped
