67e00b0f6f334b6459903588a83f6cb0ed4e4b6a
exel14 <Showmetheway220@gmail.com>
2020-12-09 17:16:44 +0600

    Change documentsetializer

--- ChamberOfSecret/HP/serializer.py
+++ ChamberOfSecret/HP/serializer.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
-        fields = ['name','date_created']
+        fields = ['name','date_created','mark','text']
 
 
 class AccountRegisterSerializer(serializers.ModelSerializer):
